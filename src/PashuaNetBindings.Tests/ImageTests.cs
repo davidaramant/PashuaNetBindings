7c2e1c58d60af7cb90610003f6cd0c2a886c7956
David Aramant <davidaramant@gmail.com>
2020-10-25 18:44:10 -0400

    Validation setting dimensions of Image

--- src/PashuaNetBindings.Tests/ImageTests.cs
+++ src/PashuaNetBindings.Tests/ImageTests.cs
@@ -16,5 +16,95 @@ namespace Pashua.Tests
             };
             image.GetValidationIssues().Should().HaveCount(1);
         }
+
+        [Fact]
+        public void ShouldValidateHeightIsPositive()
+        {
+            var image = new Image
+            {
+                Path = "path",
+                Height = -1,
+            };
+            image.GetValidationIssues().Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void ShouldValidateMaxWidthIsPositive()
+        {
+            var image = new Image
+            {
+                Path = "path",
+                MaxWidth = -1,
+            };
+            image.GetValidationIssues().Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void ShouldValidateMaxHeightIsPositive()
+        {
+            var image = new Image
+            {
+                Path = "path",
+                MaxHeight = -1,
+            };
+            image.GetValidationIssues().Should().HaveCount(1);
+        }
+
+        [Theory]
+        [InlineData(false, false)]
+        [InlineData(false, true)]
+        [InlineData(true, false)]
+        [InlineData(true, true)]
+        public void ShouldNotAllowSettingDimensionAndMaxDimensionAtSameTime(bool setWidth, bool setMaxWidth)
+        {
+            var image = new Image
+            {
+                Path = "path",
+            };
+
+            if (setWidth)
+            {
+                image.Width = 1;
+            }
+            else
+            {
+                image.Height = 1;
+            }
+
+            if (setMaxWidth)
+            {
+                image.MaxWidth = 1;
+            }
+            else
+            {
+                image.MaxHeight = 1;
+            }
+
+            image.GetValidationIssues().Should().HaveCount(1);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void ShouldAllowSettingDimensionOrMaxDimension(bool setMaxDimension)
+        {
+            var image = new Image
+            {
+                Path = "path",
+            };
+
+            if (setMaxDimension)
+            {
+                image.MaxHeight = 1;
+                image.MaxWidth = 1;
+            }
+            else
+            {
+                image.Height = 1;
+                image.Width = 1;
+            }
+
+            image.GetValidationIssues().Should().BeEmpty();
+        }
     }
 }
--- src/PashuaNetBindings/Image.cs
+++ src/PashuaNetBindings/Image.cs
@@ -10,6 +10,13 @@ namespace Pashua
             {
                 errors.Add("Image Path must be specified.");
             }
+
+            var dimensionSpecified = Width != null || Height != null;
+            var maxDimensionSpecified = MaxWidth != null || MaxHeight != null;
+            if (dimensionSpecified && maxDimensionSpecified)
+            {
+                errors.Add("Image Width/Height cannot be specified if MaxWidth/MaxHeight is specified.");
+            }
         }
     }
 }
