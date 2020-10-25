95ed1da32860c810b782dcc5bf99d189149e53c4
David Aramant <davidaramant@gmail.com>
2020-10-25 18:28:49 -0400

    Image path validation

---
+++ src/PashuaNetBindings.Tests/ImageTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Pashua.Tests
+{
+    public class ImageTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void ShouldValidatePath(string invalidValue)
+        {
+            var image = new Image
+            {
+                Path = invalidValue,
+            };
+            image.GetValidationIssues().Should().HaveCount(1);
+        }
+    }
+}
---
+++ src/PashuaNetBindings/Image.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Pashua
+{
+    public sealed partial class Image
+    {
+        partial void AdditionalValidation(List<string> errors)
+        {
+            if (string.IsNullOrWhiteSpace(Path))
+            {
+                errors.Add("Image Path must be specified.");
+            }
+        }
+    }
+}
