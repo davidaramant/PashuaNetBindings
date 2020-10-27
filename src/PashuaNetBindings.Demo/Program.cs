06633c75bfe2919227043d41f9d9eeccee540921
David Aramant <davidaramant@gmail.com>
2020-10-26 22:18:46 -0400

    WIP on demos

---
+++ src/PashuaNetBindings.Demo/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Pashua;
+using Pashua.ScriptExtensions;
+
+namespace PashuaNetBindings.Demo
+{
+    internal class Program
+    {
+        private const bool ShouldContinue = true;
+
+        private static void Main(string[] args)
+        {
+            Page? page = Page.DemoOverview;
+
+            Page? ShowDemoPage(Func<bool> showPage)
+            {
+                return showPage() == ShouldContinue ? (Page?)Page.DemoOverview : null;
+            }
+
+            while (page.HasValue)
+            {
+                switch (page)
+                {
+                    case Page.DemoOverview:
+                        page = ShowDemoSelection();
+                        break;
+                    case Page.Buttons:
+                        page = ShowDemoPage(ShowButtons);
+                        break;
+                }
+            }
+        }
+
+        enum Page
+        {
+            DemoOverview,
+            Buttons,
+        }
+
+        static Page? ShowDemoSelection()
+        {
+            var pageOptions = ((Page[])Enum.GetValues(typeof(Page))).Where(p => p != Page.DemoOverview)
+                .Select(p => p.ToString());
+
+            var script = new List<IPashuaControl>();
+            var option = script.AddAndReturn(
+                new ComboBox
+                {
+                    Label = "Select demo page to view:",
+                    Options = pageOptions,
+                });
+            var cancel = script.AddAndReturn(new CancelButton { Label = "Quit" });
+            var ok = script.AddAndReturn(new DefaultButton { Label = "Show Page" });
+
+            Script.Run(script);
+
+            if (cancel.WasClicked)
+            {
+                return null;
+            }
+            else
+            {
+                return Enum.TryParse(typeof(Page), option.SelectedOption, out object page) ? (Page?)page : null;
+            }
+        }
+
+        static bool ShowButtons()
+        {
+            var script = new List<IPashuaControl>
+            {
+                new Window { Title = "Button Demos"},
+                new Text { Label = "Various button functionality."},
+                new Button {Label = "Has Tooltip", Tooltip = "Multiline\nTooltip"},
+                new Button { Label="Disabled", Disabled = true},
+            };
+
+            var cancel = script.AddAndReturn(new CancelButton { Label = "Quit" });
+
+            Script.Run(script);
+
+            return !cancel.WasClicked;
+        }
+    }
+}
---
+++ src/PashuaNetBindings/ScriptExtensions/ScriptHelperExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Pashua.ScriptExtensions
+{
+    public static class ScriptHelperExtensions
+    {
+        /// <summary>
+        /// Adds the given control to the script and returns it.
+        /// </summary>
+        /// <typeparam name="T">The type of control being added.</typeparam>
+        /// <param name="script">The script.</param>
+        /// <param name="control">The control being added.</param>
+        /// <returns>The control.</returns>
+        public static T AddAndReturn<T>(this ICollection<IPashuaControl> script, T control) where T : IPashuaControl
+        {
+            script.Add(control);
+            return control;
+        }
+    }
+}
